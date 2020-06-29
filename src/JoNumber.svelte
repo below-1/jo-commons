eaa4394b33028e68dff7593e47b547d2aa529732
jordan meta <irnawatyully@gmail.com>
2020-06-29 14:29:10 +0800

    base

---
+++ src/JoNumber.svelte
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+  export let value;
+  export let placeholder = '';
+  export let min = null;
+  export let max = null;
+  export let step = 1;
+</script>
+
+<input
+  bind:value={value}
+  type='number'
+  min={min ? min : undefined}
+  max={max ? max : undefined}
+  step={step}
+  placeholder={placeholder}
+  class="bg-white border-gray-400 border p-2 py-1 text-sm font-semibold rounded"
+/>
---
+++ src/index.js
@@ -0,0 +1,13 @@
+export { default as JoAsyncContent } from './JoAsyncContent.svelte';
+export { default as JoAutoComplete } from './JoAutoComplete.svelte';
+export { default as JoButton } from './JoButton.svelte';
+export { default as JoDialog } from './JoDialog.svelte';
+export { default as JoErrorPane } from './JoErrorPane.svelte';
+export { default as JoInput } from './JoInput.svelte';
+export { default as JoLink } from './JoLink.svelte';
+export { default as JoNotification } from './JoNotification.svelte';
+export { default as JoNumber } from './JoNumber.svelte';
+export { default as JoRadioGroup } from './JoRadioGroup.svelte';
+export { default as JoSelect } from './JoSelect.svelte';
+export { default as JoSpinner } from './JoSpinner.svelte';
+export { default as JoWarn } from './JoWarn.svelte';
